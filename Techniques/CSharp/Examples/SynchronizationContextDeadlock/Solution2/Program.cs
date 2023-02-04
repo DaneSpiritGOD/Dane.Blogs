7b42987ee9d523467e57d8b3a62a4ca6eaaf7fd1
Dane <838652739@qq.com>
2023-02-04 12:38:19 +0000

    Solution2

---
+++ Techniques/CSharp/Examples/SynchronizationContextDeadlock/Solution2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading;
+using DispatcherModel;
+using Workload;
+
+Console.Out.WriteLine("Adopting solution 2");
+
+var window = new MyWindow();
+window.Showed += delegate {
+    var currentSynchronizationContext = SynchronizationContext.Current;
+    SynchronizationContext.SetSynchronizationContext(null);
+
+    try
+    {
+        var myjob = new MyJob();
+        myjob.DoWork(10);
+    }
+    finally
+    {
+        SynchronizationContext.SetSynchronizationContext(currentSynchronizationContext);
+    }
+};
+
+MyApplication.Run(window);
