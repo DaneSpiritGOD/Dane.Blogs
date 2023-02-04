dccb6c77e530b650737f8f21584f8bfa031f5e1a
Dane <838652739@qq.com>
2023-02-04 12:30:06 +0000

    Solution1

---
+++ Techniques/CSharp/Examples/SynchronizationContextDeadlock/Solution1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DispatcherModel;
+using Workload;
+
+Console.Out.WriteLine("Adopting solution 1");
+
+var window = new MyWindow();
+window.Showed += delegate {
+    var myjob = new MyJobWrapper();
+    myjob.DoWork(10);
+};
+
+MyApplication.Run(window);
+
+class MyJobWrapper : MyJob
+{
+    protected override async Task<int> DoWorkAsync(int seed)
+    {
+        // await Task.Delay(0).ConfigureAwait(false); // not work
+
+        await Task.Delay(1).ConfigureAwait(false); // work, but it will release execution priority
+        return await base.DoWorkAsync(seed);
+    }
+}
