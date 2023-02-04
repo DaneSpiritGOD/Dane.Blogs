3fa10ab1d6fcce3989de3fb99e5642bb4b470521
Dane <838652739@qq.com>
2023-02-04 10:43:04 +0000

    TypicalCase

---
+++ Techniques/CSharp/Examples/SynchronizationContextDeadlock/TypicalCase/MySynchronizationContext.cs
@@ -0,0 +1,48 @@
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace TypicalCase;
+
+class MySynchronizationContext : SynchronizationContext
+{
+    readonly ConcurrentQueue<(Action<object?>, object?)> queue;
+    readonly TextWriter tw;
+
+    public MySynchronizationContext(ConcurrentQueue<(Action<object?>, object?)> queue, TextWriter tw)
+    {
+        this.queue = queue;
+        this.tw = tw;
+    }
+
+    public override SynchronizationContext CreateCopy()
+    {
+        tw.WriteLine($"{nameof(CreateCopy)} being called");
+        return new MySynchronizationContext(queue, tw);
+    }
+
+    public override void OperationStarted()
+    {
+        tw.WriteLine($"{nameof(OperationStarted)} being called");
+    }
+
+    public override void OperationCompleted()
+    {
+        tw.WriteLine($"{nameof(OperationCompleted)} being called");
+    }
+
+    public override void Post(SendOrPostCallback d, object? state)
+    {
+        tw.WriteLine($@"-------------------- {nameof(Post)} being called --------------------
+{Environment.StackTrace}");
+        queue.Enqueue((s => d(s), state));
+        tw.WriteLine($"-------------------- {nameof(Post)} call exiting --------------------");
+    }
+
+    public override void Send(SendOrPostCallback d, object? state)
+    {
+        tw.WriteLine($@"-------------------- {nameof(Send)} being called --------------------
+{Environment.StackTrace}");
+        d(state);
+        tw.WriteLine($"-------------------- {nameof(Send)} call exiting --------------------");
+    }
+}
---
+++ Techniques/CSharp/Examples/SynchronizationContextDeadlock/TypicalCase/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Concurrent;
+using TypicalCase;
+
+var tw = Console.Out;
+
+var previousSynchronizationContext = SynchronizationContext.Current;
+try
+{
+    ConcurrentQueue<(Action<object?>, object?)> queue = new();
+    var mySynchronizationContext = new MySynchronizationContext(queue, tw);
+    SynchronizationContext.SetSynchronizationContext(mySynchronizationContext);
+
+    mySynchronizationContext.Post(s =>
+    {
+        tw.WriteLine("******************** Before GetResult call ********************");
+        var result = DoWorkAsync((int)s!).GetAwaiter().GetResult();
+        tw.WriteLine($"******************** After GetResult call. Result = {result} ********************");
+    }, 13);
+
+    while (true)
+    {
+        if (!queue.TryDequeue(out var item))
+        {
+            Thread.Sleep(100);
+            continue;
+        }
+
+        item.Item1(item.Item2);
+    }
+}
+finally
+{
+    SynchronizationContext.SetSynchronizationContext(previousSynchronizationContext);
+}
+
+async Task<int> DoWorkAsync(int seed)
+{
+    seed *= 12;
+
+    var resultOnTask = await Task.Run(() =>
+    {
+        var sum = 0;
+        for (var i = 0; i < 10000; ++i)
+        {
+            sum += i;
+        }
+        return sum;
+    });
+    return seed + resultOnTask / 10;
+}
